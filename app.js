33c405f9a6e968e445785506258623e193af1332
Gergo Ertli <egergo@gmail.com>
2015-07-29 16:49:06 +0200

    Simple Battle.Net authentication

--- app.js
+++ app.js
@@ -1,11 +1,78 @@
 var express = require('express');
 
+var passport = require('passport');
+var request = require('request-promise');
+var Promise = require('bluebird');
+
 var log = require('./log');
 
 var app = express();
 app.use(log.requestLogger());
 app.enable('trust proxy');
 app.disable('x-powered-by');
+
+app.use(passport.initialize());
+
+
+
+var BnetStrategy = require('passport-bnet').Strategy;
+
+// Use the BnetStrategy within Passport.
+passport.use(new BnetStrategy({
+    clientID: process.env.BNET_ID,
+    clientSecret: process.env.BNET_SECRET,
+    callbackURL: "https://egergo.localtunnel.me/auth/bnet/callback",
+    scope: ['wow.profile'],
+    region: 'eu'
+}, function(accessToken, refreshToken, profile, done) {
+	console.log('auth', accessToken, refreshToken, profile);
+
+    return done(null, profile, {accessToken: accessToken});
+}));
+
+function refreshToons(profile, accessToken) {
+
+	return request({
+		uri: 'https://eu.api.battle.net/wow/user/characters?access_token=' + encodeURIComponent(accessToken),
+		gzip: true
+	}).then(function(res) {
+
+		realms = {};
+		res = JSON.parse(res);
+		var characters = res.characters.map(function(character) {
+			realms[realmToSlug(character.realm)] = true;
+			return {
+				name: character.name,
+				realm: character.realm
+			}
+		});
+
+
+		console.log(realms);
+		console.log(characters);
+	});
+
+}
+
+function realmToSlug(realmName) {
+	return realmName.toLowerCase();
+}
+
+
+app.get('/auth/bnet', passport.authenticate('bnet'));
+
+app.get('/auth/bnet/callback', function(req, res) {
+
+	passport.authenticate('bnet', function(err, profile, info) {
+		Promise.resolve().then(function() {
+			console.log('authenticate', err, profile, info, arguments);
+			refreshToons(profile, info.accessToken);
+		});
+	})(req, res);
+
+  res.redirect('/');
+});
+
 
 
 // log errors
